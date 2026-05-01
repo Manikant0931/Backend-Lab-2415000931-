aca0700885042e4de331caf86594a68504a8c3bf
Manikant0931 <manimishra2006@gmail.com>
2026-05-01 14:31:37 +0530

    user loged in...

--- server.js
+++ server.js
@@ -5,4 +5,33 @@ const cookieParser=require("cookie-parser");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
+app.use(session({
+    secret:"PRIVATEKEY123",
+    resave:false,
+    saveUninitialized:true
+}));
+
+app.get("/",(req, res)=>{
+  if (req.cookies.user && !req.session.user) {
+    res.send("welcome back"+ req.cookies.user);
+  } else{
+    res.send("welcome");
+  }
+});
+
+app.post("/login",(req,res)=>{
+  req.session.user={
+    name:req.body.username,
+    role:req.body.role,
+  };
+  res.cookie("user",req.body.username);
+  res.send("user is logged in");
+});
+
+
+
+
+
